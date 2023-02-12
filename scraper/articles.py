2702e57b791d44b8338a879ed1a509c76b574fb6
lazarenko <lazarenko@google.com>
2023-02-12 21:37:32 +0200

    refactor and async

--- scraper/articles.py
+++ scraper/articles.py
@@ -1,16 +1,28 @@
+import asyncio
+
 from scraper.database import session
 from scraper.db_handlers import get_articles, update_article, get_tags
 from scraper.helpers import retry_get
 from scraper.settings import base_url
 
+from requests_html import HTMLResponse
 
-def parse_article(article, resp):
+
+def parse_article(article: object, resp: HTMLResponse) -> object:
+    """
+    Parses an article's title, tags, date, and content from the provided HTML
+    response.
+
+    :param article: the article object to be updated
+    :param resp: the HTML response of the article's page
+    :return: the updated article object
+    """
     article_container = resp.html.find('article', first=True)
 
     article.title = resp.html.find('h1.tm-article-snippet__title',
                                    first=True).text
     article.tags = process_tags(
-            resp.html.find('div.tm-article-snippet__hubs', first=True).links)
+        resp.html.find('div.tm-article-snippet__hubs', first=True).links)
     article.date = resp.html.find('span.tm-article-snippet__datetime-published',
                                   first=True).text
     article.content = article_container.find('div#post-content-body',
@@ -18,28 +30,64 @@ def parse_article(article, resp):
     return article
 
 
-def process_tags(tags):
+def process_tags(tags: list) -> list:
+    """
+    Returns the list of tags from the provided list of tag links.
+
+    :param tags: the list of tag links
+    :return: the list of tags
+    """
     if tags:
         return get_tags(session, [tag.split('/')[-2] for tag in tags])
 
 
-def process_article(article):
-    resp = retry_get(base_url + article.link)
+async def process_article(article: object) -> object:
+    """
+    Processes an article by retrieving its HTML response, parsing its content,
+     and updating it in the database.
+
+    :param article: the article object to be processed
+    :return: the processed article object
+    """
+    resp = await retry_get(base_url + article.link)
     article = parse_article(article, resp)
     update_article(session, article)
     return article
 
 
-def is_not_processed(article):
+def is_not_processed(article: object) -> bool:
+    """
+    Returns True if the article's content is None, False otherwise.
+
+    :param article: the article object to be checked
+    :return: True if the article's content is None, False otherwise
+    """
     return article.content is None
 
 
-def is_not_company_blog(article):
+def is_not_company_blog(article: object) -> bool:
+    """
+    Returns True if the article's link does not contain the word "company",
+    False otherwise.
+
+    :param article: the article object to be checked
+    :return: True if the article's link does not contain the word "company",
+    False otherwise
+    """
     return 'company' not in article.link
 
 
-if __name__ == '__main__':
+processed_articles = set()
+
+
+async def process_articles():
     for article in get_articles(session):
-        if is_not_processed(article) and is_not_company_blog(article):
-            article = process_article(article)
+        if is_not_processed(article) and is_not_company_blog(
+                article) and article.link not in processed_articles:
+            article = await process_article(article)
+            processed_articles.add(article.link)
             print(article.title)
+
+
+if __name__ == '__main__':
+    asyncio.run(process_articles())
--- scraper/helpers.py
+++ scraper/helpers.py
@@ -1,16 +1,33 @@
+from typing import Optional
 from requests import RequestException, ReadTimeout
-from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
 
 from scraper.settings import headers
 
-session = HTMLSession()
-session.headers.update(headers)
 
+async def retry_get(url: str, retries: Optional[int] = 3,
+                    timeout: Optional[int] = 5):
+    """
+    A function that performs an asynchronous GET request to the specified URL,
+     with optional retries and timeout.
+    If the request fails, the function will retry up to `retries` times, with a
+    `timeout` specified in seconds.
 
-def retry_get(url, retries=3, timeout=5):
+    :param url: The URL to perform the GET request to.
+    :param retries: The number of retries to perform before raising an
+    exception. Defaults to 3.
+    :param timeout: The number of seconds to wait for a response before raising
+    a ReadTimeout exception. Defaults to 5.
+    :return: A `Response` object containing the response to the GET request.
+    :raises: RequestException if the request failed, ReadTimeout if the request
+    timed out.
+    """
+    asession = AsyncHTMLSession()
+    asession.headers.update(headers)
+
     for i in range(retries):
         try:
-            response = session.get(url, timeout=timeout)
+            response = await asession.get(url, timeout=timeout)
             response.raise_for_status()
             return response
         except (RequestException, ReadTimeout) as e:
@@ -18,5 +35,3 @@ def retry_get(url, retries=3, timeout=5):
                 i -= 1
             else:
                 raise e
-
-
